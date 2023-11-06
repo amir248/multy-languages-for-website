ed4b0cde58a68cd6be5366016fb4f923c6831d6a
chikchicly <chikchicly@gmail.com>
2023-11-07 05:44:54 +0700

    07

--- multy-languages.js
+++ multy-languages.js
@@ -3,63 +3,69 @@ const boxFieldset=document.createElement("fieldset");
 boxFieldset.setAttribute('id','boxFieldset');
 document.querySelector('main').prepend(boxFieldset);
 //box fieldset
-
 const label=document.createElement('legend');
 label.innerHTML='language';
 document.querySelector('fieldset').append(label);
-
-//eng
-const checkInput=document.createElement('input');
-checkInput.type = 'checkbox';
-checkInput.name = 'checkLanguageEng';
-document.querySelector('fieldset').append(checkInput);
-
-const switchLanguage=document.createElement('span');
-switchLanguage.innerHTML=`eng`;
-document.querySelector('fieldset').append(switchLanguage);
-
-//rus
-const checkInputRus=document.createElement('input');
-checkInputRus.type = 'checkbox';
-checkInputRus.name = 'checkLanguageRus';
-document.querySelector('fieldset').append(checkInputRus);
-const switchLanguageRus=document.createElement('span');
-switchLanguageRus.innerHTML=`rus`;
-document.querySelector('fieldset').append(switchLanguageRus);
-
 //lat
+const labelLat=document.createElement('label');
+document.querySelector('fieldset').prepend(labelLat);
 const checkInputLat=document.createElement('input');
 checkInputLat.type = 'checkbox';
 checkInputLat.name = 'checkLanguageLat';
-document.querySelector('fieldset').append(checkInputLat);
+document.querySelector('fieldset > label').append(checkInputLat);
 const switchLanguageLat=document.createElement('span');
 switchLanguageLat.innerHTML=`lat`;
-document.querySelector('fieldset').append(switchLanguageLat);
-
+document.querySelector('fieldset > label').append(switchLanguageLat);
+//rus
+const labelRus=document.createElement('label');
+document.querySelector('fieldset').prepend(labelRus);
+const checkInputRus=document.createElement('input');
+checkInputRus.type = 'checkbox';
+checkInputRus.name = 'checkLanguageRus';
+document.querySelector('fieldset > label').append(checkInputRus);
+const switchLanguageRus=document.createElement('span');
+switchLanguageRus.innerHTML=`rus`;
+document.querySelector('fieldset >  label').append(switchLanguageRus);
+//eng
+const labelEng=document.createElement('label');
+document.querySelector('fieldset').prepend(labelEng);
+const checkInput=document.createElement('input');
+checkInput.type = 'checkbox';
+checkInput.name = 'checkLanguageEng';
+document.querySelector('fieldset > label').append(checkInput);
+const switchLanguage=document.createElement('span');
+switchLanguage.innerHTML=`eng`;
+document.querySelector('fieldset > label').append(switchLanguage);
+//create fieldset and input finished O_o but it's would have been better to write with hands in document.html ^_^
 // select language
 boxFieldset.elements.checkLanguageEng.addEventListener('click',()=>{
   // console.log('Eng');
-  window.location.search='?eng';
   boxFieldset.elements.checkLanguageLat.checked=false;
   boxFieldset.elements.checkLanguageRus.checked=false;
   languageSelected();
+  // window.location.search='?eng';//reloads
+  history.pushState(null,null, '?eng');
+
 });
 boxFieldset.elements.checkLanguageLat.addEventListener('click',()=>{
   // console.log('Lat');
-  window.location.search='?lat';
   boxFieldset.elements.checkLanguageEng.checked=false;
   boxFieldset.elements.checkLanguageRus.checked=false;
   languageSelectedLat();
+  // window.location.search='?lat';//reloads
+  history.pushState(null,null, '?lat');
 });
 boxFieldset.elements.checkLanguageRus.addEventListener('click',()=>{
   // console.log('Rus');
-  window.location.search='?rus';
   boxFieldset.elements.checkLanguageLat.checked=false;
   boxFieldset.elements.checkLanguageEng.checked=false;
   languageSelectedRus();
-  // useLocation();
+  // window.location.search='?rus'; //reloads
+  history.pushState(null,null, '?rus');
 });
 function languageSelected(){
+  document.querySelector('head > meta[name="description"]').setAttribute('content','switching languages in pure Javascript, multi-language script for a website, one-pager, landing page.');
+  document.title='multy language script for website';
   for(let j=0;j<=document.querySelectorAll('#eng').length;j++){
     if(boxFieldset.elements.checkLanguageEng.checked==true){
       if(document.querySelectorAll('#rus')[j]==null){
@@ -79,6 +85,8 @@ function languageSelected(){
   }
 }
 function languageSelectedLat(){
+  document.querySelector('head > meta[name="description"]').setAttribute('content','mutandi linguas in pura Javascript, multi-language script pro a website, one-pager, pagina.');
+  document.title='multa lingua scriptum pro website';
   for(let j=0;j<=document.querySelectorAll('#lat').length;j++){
     if(boxFieldset.elements.checkLanguageLat.checked==true){
       if(document.querySelectorAll('#rus')[j]==null){
@@ -98,6 +106,8 @@ function languageSelectedLat(){
   }
 }
 function languageSelectedRus(){
+  document.querySelector('head > meta[name="description"]').setAttribute('content','переключение языков на чистом яваскрипте, мультиленговый скрипт для сайта, одностраничника, кредитный пейдж.');
+  document.title='многоязычный скрипт для сайта';
   for(let j=0;j<=document.querySelectorAll('#rus').length;j++){
   if(boxFieldset.elements.checkLanguageRus.checked==true){
       if(document.querySelectorAll('#eng')[j]==null){
@@ -116,7 +126,7 @@ function languageSelectedRus(){
     }//ifMainBox
   }//for
 }//funcion
-window.addEventListener('DOMContentLoaded',useLocation());
+window.addEventListener('DOMContentLoaded',useLocation);
 function useLocation(){
   if(window.location.search=="?eng"){
     boxFieldset.elements.checkLanguageEng.checked=true;
